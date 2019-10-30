80534d7cfc4471dfa7a8d691bc2c91d04d322165
Vuk Isic <vukisic@uns.ac.rs>
2019-10-30 17:37:36 +0100

    Update

---
+++ gPRC_Demo_TC/gPRCService/Services/CustomerService.cs
@@ -0,0 +1,67 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gPRCService.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            return Task.FromResult(new CustomerModel()
+            {
+                Age = 22,
+                EmailAddress = "pera@pera.com",
+                FirstName = "Pera",
+                LastName = "Pera",
+                IsAlive = true
+            });
+        }
+
+        public override async Task GetCustomers(NewCustomersRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            var output = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    Age = 5,
+                    LastName = "Zika",
+                    EmailAddress = "zika@zika.com",
+                    IsAlive = true,
+                    FirstName = "Zika"
+                },
+                new CustomerModel
+                {
+                    Age = 5,
+                    LastName = "Zika",
+                    EmailAddress = "zika@zika.com",
+                    IsAlive = true,
+                    FirstName = "Zika"
+                },
+                new CustomerModel
+                {
+                    Age = 5,
+                    LastName = "Zika",
+                    EmailAddress = "zika@zika.com",
+                    IsAlive = true,
+                    FirstName = "Zika"
+                }
+
+            };
+
+            foreach (var item in output)
+            {
+               await responseStream.WriteAsync(item);
+            }
+        }
+    }
+}
---
+++ gPRC_Demo_TC/gRPClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using gPRCService;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest() { Name = "Vuk" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var replay = await client.SayHelloAsync(input);
+            //Console.WriteLine(replay.Message);
+
+            var request = new CustomerLookupModel() { UserId = 1 };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(channel);
+            var replay = await client.GetCustomerInfoAsync(request);
+            Console.WriteLine($"{replay.FirstName}-{replay.LastName}-{replay.IsAlive}-{replay.EmailAddress}");
+            
+            using(var call = client.GetCustomers(new NewCustomersRequest()))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var cust = call.ResponseStream.Current;
+                    Console.WriteLine($"{cust.FirstName}-{cust.LastName}-{cust.IsAlive}-{cust.EmailAddress}");
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
